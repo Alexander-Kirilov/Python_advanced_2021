6f41330a0223fb4c42c81440bae34639d33cde17
Alexander-Kirilov <alexander_kirilov@abv.bg>
2021-08-01 17:04:13 +0300

    Decorators

---
+++ OOP/Exercise/decorators/04_type_check.py
@@ -0,0 +1,18 @@
+def type_check(expected_type):
+    def decorator(function):
+        def wrapper(param):
+            result = function(param)
+            if type(param) == expected_type:
+                return result
+            return "Bad Type"
+        return wrapper
+    return decorator
+
+
+@type_check(int)
+def times2(num):
+    return num*2
+
+
+print(times2(2))
+print(times2('Not A Number'))
